df2e189ab56e5f5342f9f774db4e22ee1a8e85fb
mka <mka@A23PC11>
2023-07-10 19:30:10 +0300

    Add project files.

---
+++ WebAppRazorPagesPV114/Pages/Movies/ListMovies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazorPagesPV114.Pages.Movies
+{
+    public class ListMoviesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
